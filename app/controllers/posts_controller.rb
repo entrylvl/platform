6fb1d599bb7f3b8fd8589f283b4d0548550960f6
CarolPC <carolina.pascale.c@gmail.com>
2017-05-19 22:18:52 -0300

    Create controller CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,50 @@
+class PostsController < ApplicationController
+  before_action :set_post, only[:show, :update, :destroy, :edit]
+
+  def index
+    @posts = Posts.all
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+    def set_post
+        @post = Post.find(params[:id])
+    end
+
+    def post_params
+        params.require(:post).permit([:title, :slug, :body, :lead])
+    end
+
+end

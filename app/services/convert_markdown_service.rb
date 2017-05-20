ef10355930722f7392c8c1bb4b866f7e742826a7
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-05-19 23:31:24 -0300

    Add markdown convert service

---
+++ app/services/convert_markdown_service.rb
@@ -0,0 +1,15 @@
+class ConvertMarkdownService
+  def convert_post(post, options = {})
+    html = parser.render(post.body)
+
+    new_html = if block_given?
+      yield html, options
+    end
+
+    new_html || html
+  end
+
+  def parser
+    Redcarpet::Markdown.new(CodeRayify.new, fenced_code_blocks: true)
+  end
+end

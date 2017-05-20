27ca1703650039ac37a2c60bfb06b0976516d692
CarolPC <carolina.pascale.c@gmail.com>
2017-05-20 02:42:38 -0300

    Add Shrine for image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  include PictureUploader::Attachment.new(:image)
 end
---
+++ app/uploaders/picture_uploader.rb
@@ -0,0 +1,3 @@
+class PictureUploader < Shrine
+  # plugins and uploading logic
+end

0230a9d949efaca02a8507782f5b7da1410e26d9
CarolPC <carolina.pascale.c@gmail.com>
2017-05-20 02:18:07 -0300

    Add validation for post and scope for the status enum

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,17 @@
 class Post < ApplicationRecord
+  validates :title, :body, :slug, :lead, presence: true
+
   extend FriendlyId
 
   friendly_id :title, use: :slugged
+
+  scope :draft, -> { where(status: :draft) }
+  scope :review, -> { where(status: :review) }
+  scope :approved, -> { where(status: :approved) }
+  scope :published, -> { where(status: :published) }
+
+  def publish!
+    self.status = :published
+    save
+  end
 end

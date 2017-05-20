d0d4a4e841d7b388439ad2db0e5153f114f64cf6
CaiqueMitsuoka <caique.mitsuoka@gmail.com>
2017-05-20 05:14:59 -0300

    Add post template to soares work

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,12 @@
 class HomeController < ApplicationController
+  def index
+  end
+
+  def slug
+    @post = Post.published.where(slug: params.require(:slug)).first
+
+    unless @post
+      render :index
+    end
+  end
 end

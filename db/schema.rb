6adde6275086203b35b09247c1f26470b9a3a048
Vitor Talaia <vitor.talaia.oliveira@gmail.com>
2017-05-19 21:27:20 -0300

    Add Devise gem for authentication

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
-ActiveRecord::Schema.define(version: 20170519235423) do
-
+ActiveRecord::Schema.define(version: 20170520001943) do
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
@@ -10,6 +9,25 @@ ActiveRecord::Schema.define(version: 20170519235423) do
     t.datetime "published_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
